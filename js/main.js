4c353c04ee445666517040ccc2d9445a382eeb6a
dickymicky <dickymicky@naver.com>
2017-02-18 14:01:53 +0900

    객체지향 코드로 변경

--- js/main.js
+++ js/main.js
@@ -2,20 +2,74 @@
 
 $(document).ready(function(){
 
+    /*
     var $tabMenu = $('.tab_menu li'),
         $tabContent = $('.tab_contents li'),
         oldIndex = null,
         currentIndex = 0;
 
     $tabMenu.on('click', function(){
+        //번호 갱신
         oldIndex = currentIndex;
         currentIndex = $(this).index();
 
+        //메뉴 변경 기능
         $tabMenu.not($(this)).removeClass('active');
         $(this).addClass('active');
 
+        //이미지 변경 기능
         $tabContent.eq(oldIndex).hide();
         $tabContent.eq(currentIndex).show();
     });
+    */
+
+    var tab = new TabMenu();
 
 });
+
+
+function TabMenu(){
+    this.init();
+    this.initEvent();
+}
+
+TabMenu.prototype = {
+
+    //구조 정리
+    init: function(){
+        this.$tabMenu = $('.tab_menu li');
+        this.$tabContent = $('.tab_contents li');
+        this.oldIndex = null;
+        this.currentIndex = 0;
+    },
+
+    //시작하자마자 실행할 이벤트들
+    initEvent: function(){
+        var _this = this;
+
+        this.$tabMenu.on('click', function(){
+            _this.renewIndex($(this));
+            _this.toggleMenu($(this));
+            _this.toggleContent();
+        });
+    },
+
+    //번호 갱신 기능
+    renewIndex: function($this){
+        this.oldIndex = this.currentIndex;
+        this.currentIndex = $this.index();
+    },
+
+    //메뉴 변경 기능
+    toggleMenu: function($this){
+        this.$tabMenu.not($this).removeClass('active');
+        $this.addClass('active');
+    },
+
+    //이미지 변경 기능
+    toggleContent: function(){
+        this.$tabContent.eq(this.oldIndex).hide();
+        this.$tabContent.eq(this.currentIndex).show();
+    }
+
+};
